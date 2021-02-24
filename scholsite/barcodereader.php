012113ac2b5317fcb51298d660cf4f9be6b0a051
the311thsheep <the311thsheep@users.noreply.github.com>
2021-02-24 14:21:59 +1300

    Update barcodereader.php

--- scholsite/barcodereader.php
+++ scholsite/barcodereader.php
@@ -1,4 +1,4 @@
-<!-- <div class="row">
+<div class="row">
   <video id="player" controls autoplay></video>
   <button id="capture">Capture</button>
 </div>
@@ -25,92 +25,4 @@
       // Attach the video stream to the video element and autoplay.
       player.srcObject = stream;
     });
-</script> -->
-
-<div id="camera"></div>
-   <!-- <script src="https://serratus.github.io/quaggaJS/examples/js/quagga.min.js" type="text/javascript"></script> -->
-   <script src="/static/quagga.min.js" type="text/javascript"></script>
-   <script>
-       Quagga.init({
-           numOfWorkers: 4,
-           frequency: 10,
-           locate: true,
-           inputStream : {
-               name : "Live",
-               type : "LiveStream",
-               target: document.querySelector('#camera'),
-               constraints: {
-                   width: { min: 640, ideal: 1280, max: 1920 },
-                   height: { min: 480, ideal: 720, max: 1080 },
-                   facingMode: "environment", // or user
-                   frameRate: 10,
-               }
-           },
-           decoder : {
-               readers: [
-                   'ean_reader'
-               ]
-           },
-           locator: {
-               patchSize: "medium", // x-small, small, medium, large, x-large
-           }
-       }, function(err) {
-           if (err) {
-               console.log(err);
-               return
-           }
-           console.log("Initialization finished. Ready to start");
-           Quagga.start();
-       });
-
-       Quagga.onProcessed(function(result) {
-           var drawingCtx = Quagga.canvas.ctx.overlay,
-               drawingCanvas = Quagga.canvas.dom.overlay;
-
-           if (result) {
-               if (result.boxes) {
-                   drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                   result.boxes.filter(function (box) {
-                       return box !== result.box;
-                   }).forEach(function (box) {
-                       Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-                   });
-               }
-
-               if (result.box) {
-                   Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
-               }
-
-               if (result.codeResult && result.codeResult.code) {
-                   Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-               }
-           }
-       });
-
-       Quagga.onDetected(function(result) {
-           var isbn = result.codeResult.code;
-           if (isbn.match(/^97[8|9]/)) {
-               var style = window.innerHeight > window.innerWidth ? 'width: ' + window.innerWidth + 'px;' : 'height: ' + window.innerHeight + 'px;';
-               document.getElementById('camera').innerHTML = '<img src="https://cover.openbd.jp/' + isbn + '.jpg" alt="" style="' + style + '">';
-               var params = getQueryString();
-               if (params) {
-                   var url = params.url + '?' + params.param + '=' + isbn;
-                   setTimeout(function() {
-                       location.href = url;
-                   }, 300);
-               } else {
-                   alert(isbn);
-               }
-           }
-       });
-
-       function getQueryString() {
-           if (location.search==='') return null;
-           var params = {}
-           location.search.substr(1).split('&').map(function(param) {
-               var pairs = param.split('=');
-               params[pairs[0]] = decodeURIComponent(pairs[1]);
-           });
-           return params;
-       }
-   </script>
+</script>
