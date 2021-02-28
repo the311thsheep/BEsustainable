0ad32062bb7f8d416bebec2f0d10fdbb38fb913d
ilo5414 <ilo5414@stacmail.net>
2021-03-01 10:31:31 +1300

    1/03/21
    transparent dropdown on the right side

--- scholsite/navbar.php
+++ scholsite/navbar.php
@@ -1,6 +1,6 @@
 <!-- navbar -->
 
-<nav class="navbar navbar-light">
+<nav class="navbar navbar-dark">
   <!-- Brand -->
   <a class="navbar-brand" href="home.php"><img src="images/Be_sustainable_logo1 copy.png" style="height:200px" alt=""></a>
 
@@ -10,8 +10,8 @@
   </button>
 
   <!-- Navbar links -->
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
+  <div class="dropdown-menu dropdown-menu-right" id="collapsibleNavbar">
+    <ul class="dropdown ">
       <li class="nav-item">
         <a class="nav-link" href="#">Find products</a>
       </li>
@@ -30,27 +30,3 @@
     </ul>
   </div>
 </nav>
-<!-- Just an image -->
-<!-- <nav class="navbar">
-  <a class="navbar-brand" href="#">
-    <img src="images/Be_sustainable_logo1 copy.png" width="200" height="200" alt="">
-  </a>
-
-  <div class="pos-f-t">
-  <div class="collapse" id="navbarToggleExternalContent">
-    <div class="p-4">
-      <h4 class="text-white">Collapsed content</h4>
-      <span class="text-muted">Toggleable via the navbar brand.</span>
-    </div>
-  </div>
-  <nav class="navbar navbar-dark bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </nav>
-</div>
-</nav> -->
-
-<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-</button>
