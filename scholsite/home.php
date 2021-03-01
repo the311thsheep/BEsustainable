bfbf77089433c3f6e458b7edbe923d2f4829f82a
the311thsheep <the311thsheep@users.noreply.github.com>
2021-03-02 09:37:03 +1300

    Update home.php

--- scholsite/home.php
+++ scholsite/home.php
@@ -5,7 +5,7 @@
 
 </div> -->
 
-<div class="container-fluid">
+<div class="container-fluid" >
   <div class="row">
 
     <div class="col-4 text-white">
@@ -19,14 +19,20 @@
     <div class="col-4 text-white">
       <p>sup</p>
     </div>
+</div>
+<div class="row">
 
-    <form class="form-inline my-2 my-lg-0" method="POST" action="index.php?page=searchresults" >
+
+
+
+
+    <form class="form-inline my-2 my-lg-0"  method="POST" action="index.php?page=searchresults" >
       <input class="form-control mr-sm-2" type="search" name='search' placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
 
-    </div>
 
+  </div>
 
 
 </div>
